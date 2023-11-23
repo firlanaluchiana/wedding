2d7ac7630f7a9e18273fb73acc33d1b50496c79c
Yusuf Supriadi <yusufdolenk2@gmail.com>
2023-11-23 20:46:40 +0700

    feat: add friend count AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Story;
+use App\Models\Friend;
 use App\Models\Gallery;
 use App\Models\Wedding;
 use Illuminate\Http\Request;
@@ -15,11 +16,13 @@ class AdminController extends Controller
         $weddings = Wedding::count();
         $storys = Story::count();
         $gallerys = Gallery::count();
+        $friends = Friend::count();
 
         return response()->view('admin.index', [
             'weddings' => $weddings,
             'storys' => $storys,
             'gallerys' => $gallerys,
+            'friends' => $friends
         ]);
     }
 }
